1d0640abea5e3234181e976d00b91f2e472b8043
Carlos Sánchez <carlosasp-13@hotmail.com>
2025-07-22 09:22:08 -0500

    feat: agregar ruta /auth/me y validación de token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,37 @@
-// src/auth/auth.controller.ts
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterAuthDto } from './dto/register-auth.dto';
-import { LoginDto } from './dto/login-auth.dto'; // ✅ Añadir esta línea
+import { LoginDto } from './dto/login-auth.dto';
 import { Public } from './decorators/public.decorator';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Public() // 👈 permite acceso público a esta ruta
+  @Public()
   @Post('register')
   register(@Body() dto: RegisterAuthDto) {
     return this.authService.register(dto);
   }
 
-  @Post('login')
   @Public()
+  @Post('login')
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
+  }
+
+  // ✅ NUEVA RUTA para validar token y obtener datos del usuario autenticado
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Req() req) {
+    return req.user; // Esto devuelve { id, username, role, ... } del token
   }
 }
